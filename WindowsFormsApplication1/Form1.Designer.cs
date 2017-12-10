b9a69de1c7f49e2473787d34fb50be42d3a9a33f
nmakarychev <nmakarychev@mail.ru>
2017-12-10 16:49:50 +0500

    Добавьте файлы проекта.

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnPlay = new System.Windows.Forms.Button();
+            this.btnAddMoney = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Green;
+            this.label1.Location = new System.Drawing.Point(265, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 67);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "7";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Green;
+            this.label2.Location = new System.Drawing.Point(331, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 67);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "7";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Font = new System.Drawing.Font("Modern No. 20", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.Location = new System.Drawing.Point(397, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 67);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "7";
+            // 
+            // btnPlay
+            // 
+            this.btnPlay.BackColor = System.Drawing.Color.SandyBrown;
+            this.btnPlay.Font = new System.Drawing.Font("Times New Roman", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnPlay.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.btnPlay.Location = new System.Drawing.Point(265, 149);
+            this.btnPlay.Name = "btnPlay";
+            this.btnPlay.Size = new System.Drawing.Size(192, 113);
+            this.btnPlay.TabIndex = 3;
+            this.btnPlay.Text = "Играть";
+            this.btnPlay.UseVisualStyleBackColor = false;
+            this.btnPlay.Click += new System.EventHandler(this.btnPlay_Click);
+            // 
+            // btnAddMoney
+            // 
+            this.btnAddMoney.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.btnAddMoney.ForeColor = System.Drawing.Color.Red;
+            this.btnAddMoney.Location = new System.Drawing.Point(70, 228);
+            this.btnAddMoney.Name = "btnAddMoney";
+            this.btnAddMoney.Size = new System.Drawing.Size(133, 34);
+            this.btnAddMoney.TabIndex = 4;
+            this.btnAddMoney.Text = "Пополнить баланс";
+            this.btnAddMoney.UseVisualStyleBackColor = false;
+            this.btnAddMoney.Click += new System.EventHandler(this.btnAddMoney_Click_1);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 359);
+            this.Controls.Add(this.btnAddMoney);
+            this.Controls.Add(this.btnPlay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.Button btnAddMoney;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+      
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        Random rnd = new Random();
+        int balance = 100;
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (balance<10)
+            {
+                MessageBox.Show("Необходимо пополнить баланс");
+                return;
+            }
+            balance -= 10;
+
+            label1.Text = rnd.Next(0, 10).ToString();
+            label2.Text = rnd.Next(0, 10).ToString();
+            label3.Text = rnd.Next(0, 10).ToString();
+            int count = 0;
+            if (label1.Text == "7") count++;
+            if (label2.Text == "7") count++;
+            if (label3.Text == "7") count++;
+            if (count != 0) balance +=(int) Math.Pow(10, count);
+            this.Text = " Баланс: " + balance.ToString();
+        }
+        private void btnAddMoney_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAddMoney_Click_1(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();
+
+            if (f.ShowDialog()==DialogResult.OK)
+            {
+                int amount = int.Parse(f.txtAmount.Text);
+                balance += amount;
+            }
+            f.Close();
+        }
+    }
+}
